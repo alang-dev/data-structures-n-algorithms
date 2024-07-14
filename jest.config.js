594ee4b22bc1d195ca74772390031948d81491ac
Kien Alang <alangkien96@gmail.com>
2024-07-14 15:37:33 +0700

    Config jest to show overall coverage

--- jest.config.js
+++ jest.config.js
@@ -6,4 +6,10 @@ module.exports = {
 		'^.+\\.jsx?$': 'babel-jest'
 	},
 	moduleFileExtensions: ['js'],
+	collectCoverageFrom: [            // Specify files/globs to collect coverage from
+		"src/**/*.{js,jsx,ts,tsx}",         // Include all JS, JSX, TS, and TSX files
+		"!**/node_modules/**",           // Exclude node_modules
+		"!**/vendor/**"                 // Exclude other folders as needed
+	],
+	coverageReporters: ["text", "text-summary"],
 };
